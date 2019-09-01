98156ca6b632562dba4d31eaaf68fdbd34ed5025
felipe_ssilva <felipe.dasilva@grupoboticario.com.br>
2019-08-31 22:22:41 -0300

    :sparkles: Create FizzBuzz

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,63 +1,24 @@
 import { expect } from 'chai'
-import { sum, sub, mult, div } from '../src/main'
+import FizzBuzz from '../src/main'
 
-describe('Calc', () => {
+describe('FizzBuzz', () => {
    
-    // Smoke tests
-    describe('Smoke tests', () => {
-
-        it('should exist the method `sum`', () => {
-            expect(sum).to.exist;
-            expect(sum).to.be.a('function');
-        });
-
-        it('should exist the method `sub`', () => {
-            expect(sub).to.exist;
-            expect(sub).to.be.a('function');
-        });
-
-        it('should exist the method `mult`', () => {
-            expect(mult).to.exist;
-            expect(mult).to.be.a('function');
-        });
-
-        it('should exist the method `div`', () => {
-            expect(div).to.exist;
-            expect(div).to.be.a('function');
-        });
-
+    it('should return `Fizz` when multiple of 3', () => {
+        expect(FizzBuzz(3)).to.be.equal('Fizz');
+        expect(FizzBuzz(6)).to.be.equal('Fizz');
     });
 
-    // Sum
-    describe('Sum', () => {
-        it('should return 4 when `sum(2,2)`', () => {
-            expect(sum(2, 2)).to.be.equal(4);
-        });
+    it('should return `Buzz` when multiple of 5', ()=> {
+        expect(FizzBuzz(5)).to.be.equal('Buzz');
+        expect(FizzBuzz(10)).to.be.equal('Buzz');
     });
 
-    // Sub
-    describe('Sub', () => {
-        it('should return 4 when `sub(6,2)`', () => {
-            expect(sub(6, 2)).to.be.equal(4);
-        });
-
-        it('should return -4 when `sub(6,10)`', () => {
-            expect(sub(6, 10)).to.be.equal(-4);
-        });
+    it('should return `FizzBuzz` when multiple of 3 and 5', ()=> {
+        expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
     });
 
-    // Mult
-    describe('Mult', () => {
-        it('should return 6 when `sub(2,3)`', () => {
-            expect(mult(2, 3)).to.be.equal(6);
-        });
-    });
-
-    // Div
-    describe('Div', () => {
-        it('should return 4 when `div(4,2)`', () => {
-            expect(div(4, 2)).to.be.equal(2);
-        });
+    it('should return the number when non-multiple', ()=> {
+        expect(FizzBuzz(7)).to.be.equal(7);
     });
 
 });
